fac38c40f838f239c80f4afdc7592f0daacc03d7
ibra.malahat <ibramila2002@gmail.com>
2023-02-01 21:21:47 +0400

    footer1

--- footers/README.md
+++ footers/README.md
@@ -1,16 +1,4 @@
-# Getting Started with Create React App
-
-## Available Scripts
-
-In the project directory, you can run:
-
-### `npm start`
-
-Runs the app in the development mode.\
-Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
-
-The page will reload when you make changes.\
-You may also see any lint errors in the console.
-
+#Footer1
 <img src="./src/assets/footer1/mobile=false.png" title="hover text">
 <img src="./src/assets/footer1/mobile=true.png" title="hover text">
+
--- footers/src/components/Footer1/Footer1.jsx
+++ footers/src/components/Footer1/Footer1.jsx
@@ -1,21 +1,43 @@
 import React from 'react'
 import "./style.scss"
+import { BiPhoneCall } from "react-icons/bi"
+import { FaFacebookF } from 'react-icons/fa'
+import { BsTwitter } from "react-icons/bs"
 function Footer1() {
   return (
     <footer>
       <div className="container">
         <ul className='footer'>
           <li className='location'>
-            <ul>
-              <h2>Our office</h2>
-              <span></span>
-              <span></span>
-              <p></p>
-            </ul>
-
+            <h2>Our office</h2>
+            <div className='phone'>
+              <BiPhoneCall style={{fontSize:"20px"}}/>
+              <span>(334) 202-4792</span>
+            </div>
+            <span>haddawy@comcast.net</span>
+            <p>998 Devonshire Ave.Camp Hill, PA 17011</p>
           </li>
-          <li></li>
-          <li></li>
+          <li>
+            <h2>Say hello</h2>
+            <form action="">
+              <input type="text" placeholder='Name' />
+              <input type="text" placeholder='Telephone' />
+              <input type="text" placeholder='Email' />
+              <textarea name="comment" id="comment" cols="25" rows="5" placeholder='Comment'></textarea>
+              <button>Submit</button>
+            </form>
+          </li>
+          <li>
+            <h2>Keep connected</h2>
+            <div style={{ display: "flex", gap: "30px" }}>
+              <BsTwitter style={{fontSize:"20px"}}/>
+              <span>Twitter</span>
+            </div>
+            <div style={{ display: "flex", gap: "30px" }}>
+              <FaFacebookF style={{fontSize:"20px"}} />
+              <span>Facebook</span>
+            </div>
+          </li>
         </ul>
       </div>
     </footer>
